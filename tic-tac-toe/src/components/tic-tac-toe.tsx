401b5999b009d362d1c913ca34d661763c85e704
avishekpadhi <abhishekpadhi69@gmail.com>
2025-11-08 22:23:15 +0530

    feat: setting up the board for a 3*3 tic tac toe

--- tic-tac-toe/src/components/tic-tac-toe.tsx
+++ tic-tac-toe/src/components/tic-tac-toe.tsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
-
-const initialBoard = () => Array(9).fill(null);
+import useTicTacToe from "../hooks/use-tic-tac-toe";
 
 function TicTacToe() {
-  const [board, setBoard] = useState(initialBoard());
+  const { board, calculateWinner, resetGame, getStatusMessage, handleClick } =
+    useTicTacToe();
   return (
-    <div className="game  max-w-[400px] mx-auto items-center">
+    <div className="game  max-w-[500px] mx-auto items-center">
       <div className="status m-5 flex justify-between">
-        Player X Turn
-        <button className="border rounded px-2 py-1">Reset Game</button>
+        {getStatusMessage()}
+        <button className="border rounded px-2 py-1" onClick={resetGame}>
+          Reset Game
+        </button>
       </div>
 
       <div className="board grid grid-cols-3 gap-2">
-        {board.map((bd, index) => (
+        {board.map((b, index) => (
           <button
-            className="cell p-2 border rounded cursor-pointer"
+            className="cell p-2 border rounded cursor-pointer h-40 w-40"
             key={index}
+            onClick={() => handleClick(index)}
+            disabled={b !== null}
           >
-            X
+            {b}
           </button>
         ))}
       </div>
